c92333b95477f3a1af6f9fec1e56f181c7e99c72
Ricardo Mendes <ricardolsmendes@gmail.com>
2023-08-14 16:16:47 -0300

    style: fix formatting and linting issues

--- src/spark/sample_spark_job.py
+++ src/spark/sample_spark_job.py
@@ -1,16 +1,15 @@
 import sys
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+# @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
+job.init(args["JOB_NAME"], args)
 job.commit()
