ce22c5542dda034b5804627daa71b3cd8c183ec0
Ricardo Mendes <ricardolsmendes@gmail.com>
2023-09-07 22:51:15 -0300

    feat: set up IaC (#4)

---
+++ infrastructure/modules/s3/variables.tf
@@ -0,0 +1,23 @@
+variable "environment" {
+  description = "The deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "data_bucket_name" {
+  description = "Name of the S3 bucket used to store data"
+  type        = string
+  default     = "aws-glue-ci-cd-blueprint-data"
+}
+
+variable "glue_assets_bucket_name" {
+  description = "Name of the S3 bucket used to store AWS Glue assets"
+  type        = string
+  default     = "aws-glue-ci-cd-blueprint-assets"
+}
+
+variable "glue_scripts_bucket_name" {
+  description = "Name of the S3 bucket used to store AWS Glue scripts"
+  type        = string
+  default     = "aws-glue-ci-cd-blueprint-scripts"
+}
