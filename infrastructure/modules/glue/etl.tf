0e1c03845e507fa8c87e61676be82d72eaa255ce
Ricardo Mendes <ricardolsmendes@gmail.com>
2023-11-26 15:57:01 -0300

    fix: update script locations and bronze resource path

--- infrastructure/modules/glue/etl.tf
+++ infrastructure/modules/glue/etl.tf
@@ -6,13 +6,13 @@ resource "aws_glue_job" "us_legislators_bronze_job" {
   worker_type       = "G.1X"
   number_of_workers = 10
   command {
-    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/sample_bronze_job.py"
+    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/s3_to_s3_job.py"
   }
   default_arguments = {
     "--source-bucket-name" = "awsglue-datasets",
     "--source-file-path"   = "examples/us-legislators/all/persons.json",
     "--target-bucket-name" = var.data_bucket_id,
-    "--target-file-path"   = "bronze/us-legislators/all-persons.json",
+    "--target-file-path"   = "bronze/awsglue-datasets/us-legislators/all-persons.json",
   }
   tags = {
     Project     = "AWS Glue CI/CD Blueprint"
@@ -28,11 +28,11 @@ resource "aws_glue_job" "us_legislators_silver_job" {
   worker_type       = "G.1X"
   number_of_workers = 10
   command {
-    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/sample_silver_job.py"
+    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/s3_json_to_parquet_job.py"
   }
   default_arguments = {
     "--source-bucket-name" = var.data_bucket_id,
-    "--source-file-path"   = "bronze/us-legislators/all-persons.json",
+    "--source-file-path"   = "bronze/awsglue-datasets/us-legislators/all-persons.json",
     "--target-bucket-name" = var.data_bucket_id,
     "--target-table-path"  = "silver/us-legislators",
   }
