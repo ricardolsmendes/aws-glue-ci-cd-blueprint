fadafdaaa6f457b86aec628f52e6877fcf0821e2
Ricardo Mendes <ricardolsmendes@gmail.com>
2023-12-05 07:47:41 -0300

    refactor: move resources from the core to the glue module (#42)

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -1,19 +1,17 @@
 module "core" {
   source = "../../modules/core"
 
-  environment              = var.environment
-  data_bucket_name         = var.data_bucket_name
-  glue_assets_bucket_name  = var.glue_assets_bucket_name
-  glue_scripts_bucket_name = var.glue_scripts_bucket_name
+  environment      = var.environment
+  data_bucket_name = var.data_bucket_name
 }
 
 module "glue" {
   source = "../../modules/glue"
 
-  environment            = var.environment
-  data_bucket_id         = module.core.data_bucket_id
-  glue_scripts_bucket_id = module.core.glue_scripts_bucket_id
-  glue_service_role_id   = module.core.glue_service_role_id
+  environment              = var.environment
+  data_bucket_id           = module.core.data_bucket_id
+  glue_assets_bucket_name  = var.glue_assets_bucket_name
+  glue_scripts_bucket_name = var.glue_scripts_bucket_name
 }
 
 module "athena" {
--- infrastructure/modules/glue/etl.tf
+++ infrastructure/modules/glue/etl.tf
@@ -1,7 +1,7 @@
 resource "aws_glue_job" "bronze_us_legislators" {
   name              = "glue-ci-cd-bronze-us-legislators-${var.environment}"
   description       = "Sample Bronze-layer job for the AWS Glue CI/CD Blueprint."
-  role_arn          = data.aws_iam_role.glue_service.arn
+  role_arn          = aws_iam_role.glue_service.arn
   glue_version      = "4.0"
   worker_type       = "G.1X"
   number_of_workers = 10
@@ -14,14 +14,14 @@ resource "aws_glue_job" "bronze_us_legislators" {
   }
   tags = local.default_tags
   command {
-    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/s3_to_s3_job.py"
+    script_location = "s3://${aws_s3_bucket.glue_scripts.id}/s3_to_s3_job.py"
   }
 }
 
 resource "aws_glue_job" "silver_us_legislators" {
   name              = "glue-ci-cd-silver-us-legislators-${var.environment}"
   description       = "Sample Silver-layer job for the AWS Glue CI/CD Blueprint."
-  role_arn          = data.aws_iam_role.glue_service.arn
+  role_arn          = aws_iam_role.glue_service.arn
   glue_version      = "4.0"
   worker_type       = "G.1X"
   number_of_workers = 10
@@ -34,6 +34,6 @@ resource "aws_glue_job" "silver_us_legislators" {
   }
   tags = local.default_tags
   command {
-    script_location = "s3://${data.aws_s3_bucket.glue_scripts.id}/s3_json_to_parquet_job.py"
+    script_location = "s3://${aws_s3_bucket.glue_scripts.id}/s3_json_to_parquet_job.py"
   }
 }
