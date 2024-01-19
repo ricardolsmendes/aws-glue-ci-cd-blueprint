eb97c9c72bfc99e946055362ac143f0dcd292112
Ricardo Mendes <ricardolsmendes@gmail.com>
2024-01-19 09:59:44 -0300

    refactor: set default tags at the provider level

--- infrastructure/environments/prod/provider.tf
+++ infrastructure/environments/prod/provider.tf
@@ -4,6 +4,7 @@ terraform {
     key    = "state/prod.tfstate"
     region = "us-east-1"
   }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,4 +17,12 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "AWS Glue CI/CD Blueprint"
+      Environment = var.environment
+      CreatedBy   = "Terraform"
+    }
+  }
 }
--- infrastructure/modules/core/s3.tf
+++ infrastructure/modules/core/s3.tf
@@ -1,4 +1,3 @@
 resource "aws_s3_bucket" "data" {
   bucket = "${var.data_bucket_name}-${var.environment}"
-  tags   = local.default_tags
 }
