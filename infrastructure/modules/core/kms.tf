03be899cc832f20b9c4ca8c1d3838e09f24f059c
Ricardo Mendes <ricardolsmendes@gmail.com>
2024-05-24 14:59:31 -0300

    docs: improve comments on SSE-KMS/S3 encryption

--- infrastructure/modules/core/kms.tf
+++ infrastructure/modules/core/kms.tf
@@ -1,9 +1,11 @@
+# This KMS key is used to implement SSE-KMS encryption in S3.
+#
 # Since January, 2023, AWS applies server-side encryption with Amazon S3 managed keys
 # (SSE-S3) as the base level of encryption for every bucket in S3. However, users can
 # choose to configure buckets to use server-side encryption with AWS Key Management
-# Service (KMS) keys (SSE-KMS) instead. This KMS key is used to implement SSE-KMS.
-# Please refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingEncryption.html
-# for further details.
+# Service keys (SSE-KMS) instead. Please refer to
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/UsingEncryption.html for further
+# details.
 resource "aws_kms_key" "s3" {
   description         = "This key that protects S3 objects tackled by the AWS Glue CI/CD Blueprint"
   enable_key_rotation = true
