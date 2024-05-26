d9e13af404cd0e0b2219a582fd9585d60f2aa0a4
Ricardo Mendes <ricardolsmendes@gmail.com>
2024-05-26 20:36:18 -0300

    chore: disable SSE-KMS encryption in S3

--- infrastructure/modules/core/s3.tf
+++ infrastructure/modules/core/s3.tf
@@ -2,13 +2,16 @@ resource "aws_s3_bucket" "data" {
   bucket = "${var.data_bucket_name}-${var.environment}"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
-  bucket = aws_s3_bucket.data.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.s3.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
+# =======================================================================================
+# KMS KEY ARE DISABLED BY DEFAULT. PLEASE REFER TO `kms.tf` FOR DETAILS.
+# =======================================================================================
+# resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
+#   bucket = aws_s3_bucket.data.id
+#
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.s3.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
--- infrastructure/modules/glue/variables.tf
+++ infrastructure/modules/glue/variables.tf
@@ -10,11 +10,14 @@ variable "data_bucket_id" {
   default     = ""
 }
 
-variable "s3_encryption_key_arn" {
-  description = "ARN of the key that protects S3 objects tackled by the AWS Glue CI/CD Blueprint."
-  type        = string
-  default     = ""
-}
+# =======================================================================================
+# DELETE THIS AND UNCOMMENT THE FOLLOWING VARIABLE TO ENABLE SSE-KMS ENCRYPTION IN S3.
+# =======================================================================================
+# variable "s3_encryption_key_arn" {
+#   description = "ARN of the key that protects S3 objects tackled by the AWS Glue CI/CD Blueprint."
+#   type        = string
+#   default     = ""
+# }
 
 variable "glue_assets_bucket_name" {
   description = "Name of the S3 bucket used to store AWS Glue assets."
